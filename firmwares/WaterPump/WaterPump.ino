d969ef0d07a29e28580560442dc17eee21e7e1af
Stanislav Bobokalo <stas.gurland@gmail.com>
2021-01-16 02:59:57 +0200

    Add Doxygen comments for docs

--- firmwares/WaterPump/WaterPump.ino
+++ firmwares/WaterPump/WaterPump.ino
@@ -5,41 +5,58 @@
 #include <EEPROM.h>
 #include <buttonMinim.h>
 
-#define PIN_RELAY 3
+#define PIN_RELAY 3 ///< Relay pin number
 
-#define PIN_LEFT_BTN 7
-#define PIN_SELECT_BTN 6
-#define PIN_RIGHT_BTN 5
+#define PIN_LEFT_BTN 7 ///< Left button pin number
+#define PIN_SELECT_BTN 6 ///< Select button pin number
+#define PIN_RIGHT_BTN 5 ///< Right button pin number
 
-#define PIN_HUMIDITY_SENSOR A0 // Analog pin 0 for soil moisture sensor
+#define PIN_HUMIDITY_SENSOR A0 ///< Analog pin 0 for soil moisture sensor
 
-#define SECONDS_OFFSET 10 // EEPROM adress offset for stroing uint32_t pumping timer
-#define PUMPING_SAFETY_INTERVAL 10000 // Minimal safe interval between pumping
+#define SECONDS_OFFSET 10 ///< EEPROM adress offset for stroing uint32_t pumping timer
+#define PUMPING_SAFETY_INTERVAL 10000 ///< Minimal safe interval between pumping
 
-buttonMinim btnLeft(PIN_LEFT_BTN);
-buttonMinim btnSelect(PIN_SELECT_BTN);
-buttonMinim btnRight(PIN_RIGHT_BTN);
+///@{
+buttonMinim btnLeft(PIN_LEFT_BTN); ///< Left button object
+buttonMinim btnSelect(PIN_SELECT_BTN); ///< Select button object
+buttonMinim btnRight(PIN_RIGHT_BTN); ///< Right button object
+///@}
 
-// Half arrow chars to display menu selection position and type (full = edit mode, hollow = selected mode)
+int8_t selectionPosition = 1; ///< Store current selection arrow position. Later used inside setSelectionArrow()
+bool selectionType = false; ///< Current selection type, affects arrow icon (false = hollow, true = full)
+
+/** @name Arrow characters
+ *  Used to draw hollow and filled arrows for UI 
+ 	(hollow = selected mode, full = edit mode)
+ */
+///@{
 uint8_t hollowLeftArrow[8] = { 0b11111, 0b01001, 0b00101, 0b00011, 0b00001, 0b00000, 0b00000, 0b00000 };
 uint8_t hollowRightArrow[8] = { 0b11111, 0b10010, 0b10100, 0b11000, 0b10000, 0b00000, 0b00000, 0b00000 };
 uint8_t fullLeftArrow[8] = { 0b11111, 0b01111, 0b00111, 0b00011, 0b00001, 0b00000, 0b00000, 0b00000 };
 uint8_t fullRightArrow[8] = { 0b11111, 0b11110, 0b11100, 0b11000, 0b10000, 0b00000, 0b00000, 0b00000 };
-
-// Hours, minutes, seconds and minimal humidity percentage displayed on screen
-// It is being written/read from EEPROM adresses 0 = hours, 1 = minutes, 2 = seconds, 3 = humidity
-int8_t hoursInterval, minutesInterval, secondsInterval, humidityPercentage, selectionPosition = 1;
-
-// Hollow arrow selection type
-bool selectionType = false;
-
-// Timers used in loop() to re-render UI, write and check eepprom time, and store last safely pumped time
-long renderTimer, eepromSecondsTimer, lastPumpingTime = 0;
-
-LiquidCrystal_I2C lcd(0x27, 20, 4);
+///@}
 
 
-int testHum = 400;
+/** @name Pumping settings
+ *  Hours, minutes, seconds, and minimal humidity percentage which are displayed on the screen and synchronized with EEPROM.
+ 	Used EEPROM offsets: 0 = hours, 1 = minutes, 2 = seconds, 3 = humidity
+ */
+///@{
+int8_t hoursInterval, minutesInterval, secondsInterval, humidityPercentage;
+///@}
+
+
+/** @name Timers
+ *  Timers used in loop(), to re-render UI, write and check eepprom time, and store last safely pumped time
+ */
+///@{
+long renderTimer; ///< UI re-render timer
+long eepromSecondsTimer; ///< Check and write pumping intervals in EEPROM
+long lastPumpingTime; ///< Store last safely pumped time
+///@}
+
+
+LiquidCrystal_I2C lcd(0x27, 20, 4); ///< LCD 2004 screen object
 
 void setup()
 {
@@ -189,36 +206,42 @@ void loop()
     }
 }
 
+/// @brief Write long int to EEPROM
+/// This function will write a 4 byte (32bit) long to the EEPROM
+/// at the specified address to adress + 3.
 void EEPROMWritelong(int address, long value)
 {
-    //Decomposition from a long to 4 bytes by using bitshift.
-    //One = Most significant -> Four = Least significant byte
+    // Decomposition from a long to 4 bytes by using bitshift.
+    // One = Most significant -> Four = Least significant byte
     byte four = (value & 0xFF);
     byte three = ((value >> 8) & 0xFF);
     byte two = ((value >> 16) & 0xFF);
     byte one = ((value >> 24) & 0xFF);
 
-    //Write the 4 bytes into the eeprom memory.
+    // Write the 4 bytes into the EEPROM memory.
     EEPROM.write(address, four);
     EEPROM.write(address + 1, three);
     EEPROM.write(address + 2, two);
     EEPROM.write(address + 3, one);
 }
 
-//This function will return a 4 byte (32bit) long from the eeprom
-//at the specified address to adress + 3.
+/// @brief Read long int from EEPROM
+/// This function will return a 4 byte (32bit) long from the EEPROM
+/// at the specified address to adress + 3.
 long EEPROMReadlong(long address)
 {
-    //Read the 4 bytes from the eeprom memory.
+    // Read the 4 bytes from the EEPROM memory.
     long four = EEPROM.read(address);
     long three = EEPROM.read(address + 1);
     long two = EEPROM.read(address + 2);
     long one = EEPROM.read(address + 3);
 
-    //Return the recomposed long by using bitshift.
+    // Return the recomposed long by using bitshift.
     return ((four << 0) & 0xFF) + ((three << 8) & 0xFFFF) + ((two << 16) & 0xFFFFFF) + ((one << 24) & 0xFFFFFFFF);
 }
 
+/// @brief Start water pumping if it's safe
+/// \returns Pumping status 
 bool startPumping()
 {
     // Starts pumping if safety interval passed
@@ -226,13 +249,7 @@ bool startPumping()
         digitalWrite(PIN_RELAY, LOW);
         delay(3000);
         digitalWrite(PIN_RELAY, HIGH);
-        lastPumpingTime = millis();
-        
-        int humidityAnalogValue = testHum - random(150, 200);
-        humidityAnalogValue = constrain(humidityAnalogValue, 0, 1023);
-        
-        testHum = humidityAnalogValue;
-        
+        lastPumpingTime = millis();        
         return true;
     }
     else {
@@ -240,17 +257,20 @@ bool startPumping()
     }
 }
 
+/// @brief Displays decimal number on the screen 
+/// @param x X coordinate
+/// @param y Y coordinate
+/// @param dec 1-2 digit integer to draw
 void decOut(int x, int y, int dec)
 {
-    // Displays number on screen at (x; y) point
     lcd.setCursor(x, y);
     lcd.print(dec / 10);
     lcd.print(dec % 10);
 }
 
+/// @brief Displays current settings of pumping interval and minimal humidity settings
 void displaySettings()
 {
-    // Displays current status of pumping interval and minimal humidity settings
     decOut(0, 1, hoursInterval);
     lcd.print(":");
     decOut(3, 1, minutesInterval);
@@ -261,30 +281,30 @@ void displaySettings()
     lcd.print("%");
 }
 
+/// @brief Displays current soil moisture on the screen
 void displayCurrentHumidity()
-{
-    // Displays current humidity from sensor
+{    
     lcd.setCursor(0, 3);
     lcd.print("Humidity - ");
     lcd.print(getHumidityPercentage());
     lcd.print("%");
 }
 
+/// @brief Get soil moisture percentage
+/// \returns Moisture value from sensor, between 0 and 99 percent
 int getHumidityPercentage()
 {
     // Reads analog pin value and maps it to 0-99% range
     int humidityAnalogValue = analogRead(PIN_HUMIDITY_SENSOR);
-    
-    // humidityAnalogValue = testHum + random(-5, 5);
-    // humidityAnalogValue = constrain(humidityAnalogValue, 0, 1023);
-    
+
     return map(humidityAnalogValue, 0, 1023, 99, 0);
 }
 
+/// @brief Draw settings arrow
+/// @param type Determines hollow (type = false) or full (type = true) arrow type
+/// @param pos Position of arrow (1 = hours, 2 = minutes, 3 = seconds, 4 = minimal humidity)
 void setSelectionArrow(bool type, int pos)
 {
-    // Sets hollow (type = false) or full (type = true) arrow at setting position
-    // Position (1 = hours, 2 = minutes, 3 = seconds, 4 = minimal humidity)
     switch (pos) {
     case 1:
         lcd.setCursor(0, 0);
@@ -310,11 +330,11 @@ void setSelectionArrow(bool type, int pos)
     }
 }
 
-void refresh(bool type, int pos)
+/// @brief Re-render whole screen
+void refresh(bool selType, int selPos)
 {
-    // Does full refresh of display
     lcd.clear();
-    setSelectionArrow(type, pos);
+    setSelectionArrow(selType, selPos);
     displaySettings();
     displayCurrentHumidity();
 }
